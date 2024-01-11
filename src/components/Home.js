5f299d8aabd754e539123f63af70c085a0813f0c
hashamuddin1 <73032957+hashamuddin1@users.noreply.github.com>
2024-01-11 23:02:28 +0500

    home screen completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,160 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import Navbar from "./Navbar";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Grid from "@mui/material/Grid";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Skeleton from "@mui/material/Skeleton";
 
 export default function Home() {
-  return <div>Home</div>;
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/login");
+    }
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_BASE_URL_V1}/api/dashBoardKPI`,
+          {
+            headers: {
+              "x-access-token": `${token}`,
+            },
+          }
+        );
+        console.log(response.data);
+        if (response.data) {
+          setData(response.data);
+          console.log(response.data);
+          setIsLoading(false);
+        }
+      } catch (error) {
+        console.log(error);
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+  if (isLoading) {
+    return (
+      <>
+        <Box sx={{ display: "flex" }}>
+          <Navbar />
+          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+            <Grid container spacing={2}>
+              {[1, 2, 3, 4, 5, 6, 7].map((item) => (
+                <Grid item key={item} xs={12} md={12} lg={4}>
+                  <Card>
+                    <Skeleton
+                      variant="rectangular"
+                      height={150}
+                      animation="wave"
+                    />
+                    <CardContent>
+                      <Typography variant="h5" component="div">
+                        <Skeleton width="80%" animation="wave" />
+                      </Typography>
+                      <Typography>
+                        <Skeleton width="60%" animation="wave" />
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        </Box>
+      </>
+    );
+  }
+  return (
+    <>
+      <Box sx={{ display: "flex" }}>
+        <Navbar />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={12} lg={4}>
+              <Card sx={{ backgroundColor: "green", color: "white" }}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    Total Users
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }}>{data.totalUsers}</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item xs={12} md={12} lg={4}>
+              <Card sx={{ backgroundColor: "blue", color: "white" }}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    Total Active List
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }}>
+                    {data.totalActiveList}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} md={12} lg={4}>
+              <Card sx={{ backgroundColor: "purple", color: "white" }}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    Total Pending List
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }}>
+                    {data.totalPendingList}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} md={12} lg={4}>
+              <Card sx={{ backgroundColor: "red", color: "white" }}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    Total Issues
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }}>{data.totalIssue}</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} md={12} lg={4}>
+              <Card sx={{ backgroundColor: "#60698A", color: "white" }}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    Total Roles
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }}>{data.totalRoles}</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} md={12} lg={4}>
+              <Card sx={{ backgroundColor: "#900C3F", color: "white" }}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    Total Turn Over
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }}>{data.totalTurnOver}</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </>
+  );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,252 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { useNavigate } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
+import Button from "@mui/material/Button";
+import PersonIcon from "@mui/icons-material/Person";
+import WorkIcon from "@mui/icons-material/Work";
+import ErrorIcon from "@mui/icons-material/Error";
+import PolicyIcon from "@mui/icons-material/Policy";
+import "../App.css";
+
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+  backgroundColor: "orangered",
+  color: "white",
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  backgroundColor: "orangered",
+  color: "white",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
+
+export default function Navbar() {
+  const [open, setOpen] = React.useState(true);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={() => setOpen(!open)}>
+            {open ? (
+              <ChevronLeftIcon sx={{ color: "white" }} />
+            ) : (
+              <ChevronRightIcon sx={{ color: "white" }} />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          <ListItem
+            disablePadding
+            sx={{ display: "block" }}
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              >
+                <HomeIcon sx={{ color: "white" }} />
+              </ListItemIcon>
+              <ListItemText primary="Home" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem
+            disablePadding
+            sx={{ display: "block" }}
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              >
+                <PersonIcon sx={{ color: "white" }} />
+              </ListItemIcon>
+              <ListItemText primary="Users" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem
+            disablePadding
+            sx={{ display: "block" }}
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              >
+                <WorkIcon sx={{ color: "white" }} />
+              </ListItemIcon>
+              <ListItemText primary="List" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem
+            disablePadding
+            sx={{ display: "block" }}
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              >
+                <ErrorIcon sx={{ color: "white" }} />
+              </ListItemIcon>
+              <ListItemText primary="Issues" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem
+            disablePadding
+            sx={{ display: "block" }}
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              >
+                <PolicyIcon sx={{ color: "white" }} />
+              </ListItemIcon>
+              <ListItemText primary="Policy" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+        </List>
+        <List sx={{ marginTop: "auto" }}>
+          <ListItem disablePadding>
+            <ListItemButton sx={{ justifyContent: "center" }}>
+              <Button
+                className="logoutBtn"
+                variant="contained"
+                sx={{
+                  backgroundColor: "white",
+                  color: "orangered",
+                  width: "50%",
+                }}
+                onClick={handleLogout}
+              >
+                Logout
+              </Button>
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Drawer>
+    </Box>
+  );
+}
